dfe86949f3de71f5b38925bf4ada780c6698b00e
Radwan Abu-Odeh <radwanizzat@gmail.com>
2020-03-28 14:02:25 +0300

    Fixed issues with Users API

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,6 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
@@ -42,8 +43,32 @@ class UserSerializer(serializers.ModelSerializer):
     ])
     role = serializers.CharField()
     phone_number = serializers.CharField(max_length=128, required=False)
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        role = validated_data.pop('role', 'employee')
+        password = validated_data.pop('password', None)
+        if not password:
+            raise ValidationError({
+                'password': _("This is a required field.")
+            })
+        user = super().create(validated_data)
+        user.role = role
+        user.set_password(password)
+        return user
+
+    def update(self, instance, validated_data):
+        role = validated_data.pop('role', 'employee')
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+
+        if role:
+            user.role = role
+        return user
 
     class Meta:
         model = User
         fields = ('id', 'first_name', 'phone_number', 'last_name',
-                  'role', 'email', 'is_active')
+                  'role', 'email', 'is_active', 'password')
