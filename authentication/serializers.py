991e0dd5476bc48bea70e31f11fa2cd390ca7640
Radwan Abu-Odeh <radwanizzat@gmail.com>
2020-03-28 14:10:06 +0300

    Fixed password setting, and updating.

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -55,6 +55,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = super().create(validated_data)
         user.role = role
         user.set_password(password)
+        user.save()
         return user
 
     def update(self, instance, validated_data):
@@ -63,6 +64,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = super().update(instance, validated_data)
         if password:
             user.set_password(password)
+            user.save()
 
         if role:
             user.role = role
